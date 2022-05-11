13cf357fec3964b4dda88a53c0ab374c3602b049
hsx <18773353532@163.com>
2022-05-11 14:16:09 +0800

    完善绩效月报

--- src/components/KPIsheet.vue
+++ src/components/KPIsheet.vue
@@ -9,7 +9,7 @@
         <el-button class="btnSelect" :icon="Search" @click="selectGradeByName()"  type="primary" round>
           <span style="vertical-align: middle" >搜索</span>
         </el-button>
-        <el-button class="btnadd" type="success" :icon="DocumentAdd" round>
+        <el-button class="btnadd" type="success" @click="dialogVisible=true" :icon="DocumentAdd" round>
           <span style="vertical-align: middle;">评分定级</span>
         </el-button>
       </div>
@@ -58,6 +58,78 @@
       <el-empty description="没有找到相关数据！" v-if="data.isShow"/>
     </div>
 
+<!--    评分定级的表单-->
+    <el-dialog
+        v-model="dialogVisible"
+        title="修改"
+        width="30%"
+        :before-close="handleClose"
+
+    >
+      <el-form :inline="true" :model="form" label-width="100px">
+        <el-form-item label="员工姓名" >
+          <el-input   v-model="form.rzname" />
+        </el-form-item>
+          <el-icon><avatar @click="findAllEmp();dialogVisible3=true" /></el-icon>
+        <el-form-item label="绩效名称" >
+          <el-input   v-model="form.jmc"/>
+        </el-form-item>
+        <el-icon><more-filled /></el-icon>
+        <el-form-item label="绩效内容" >
+          <el-input   v-model="form.jnr"/>
+        </el-form-item>
+        <el-form-item label="绩效分数" >
+          <el-input   v-model="form.jxfs"/>
+        </el-form-item>
+        <el-form-item label="绩效等级" >
+          <el-input   v-model="form.jdjb"/>
+        </el-form-item>
+      </el-form>
+        <template #footer>
+        <span class="dialog-footer">
+          <el-button @click="dialogVisible = false">关闭</el-button>
+          <el-button type="primary" @click="dialogVisible=false;open1()">保存</el-button>
+        </span>
+        </template>
+    </el-dialog>
+
+<!--    查询所有员工-->
+    <el-dialog
+        v-model="dialogVisible3"
+        title="员工列表"
+        width="30%"
+        :before-close="handleClose3"
+        :modal="form"
+    >
+      <div>
+        <el-input v-model="form.name" placeholder="请输入员工名称" style="width: 200px;margin-left: 80px" />
+        <el-button class="btnSelect" :icon="Search" @click="selectAllEmpByName" type="primary" round>
+          <span style="vertical-align: middle" >查询</span>
+        </el-button>
+      </div>
+      <el-table
+          ref="multipleTableRef"
+          :data="form.tableData"
+          style="width: 100%"
+          @current-change=""
+          highlight-current-row
+      >
+        <el-table-column type="radio" width="55"  />
+        <el-table-column label="员工编号" width="120" prop="ybh">
+          <template #default="scope">{{ scope.row.ybh }}</template>
+        </el-table-column>
+        <el-table-column label="职位编号" width="120" prop="zwbh" />
+        <el-table-column label="员工姓名" show-overflow-tooltip prop="rzname" />
+      </el-table>
+      <el-empty description="没有找到该员工！" v-if="form.isShow" style="height: 178px"/>
+      <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="dialogVisible3 = false">关闭</el-button>
+        <el-button type="primary" @click="dialogVisible3=false">确定</el-button>
+      </span>
+      </template>
+    </el-dialog>
+
     <!-- 修改绩效评分-->
     <el-dialog
         v-model="dialogVisible2"
@@ -71,6 +143,9 @@
       </el-form-item>
       <el-form-item label="绩效分数" >
         <el-input   v-model="selectForm.jxfs"/>
+      </el-form-item>
+      <el-form-item label="绩效等级" >
+        <el-input   v-model="selectForm.jdjb"/>
       </el-form-item>
       <template #footer>
       <span class="dialog-footer">
@@ -102,7 +177,8 @@ import {
   Search,
   DocumentAdd,
   Edit,
-  DocumentDelete
+  DocumentDelete,
+  Link, Avatar
 } from '@element-plus/icons-vue'
 import {ElMessageBox} from "element-plus/es";
 import {ElMessage} from "element-plus";
@@ -149,15 +225,79 @@ function selectGradeByName(){
   })
 }
 
+const dialogVisible=ref(false)
+const handleClose = (done: () => void) => {
+  ElMessageBox.confirm('确定关闭吗?')
+      .then(() => {
+        done()
+      })
+      .catch(() => {
+        // catch error
+      })
+}
+const form=reactive({
+  jxbh:0,
+  jmc:'',
+  jnr:'',
+  rzname:'',
+  jxsj:'',
+  jxfs:0,
+  jdjb:'',
+  rzbh:0,
+  zwbh:0,
+  tableData:[],
+  name:'',
+  isShow:false
+})
+// const tableData = [
+//   {
+//     date: [],
+//     name: [],
+//     address: [],
+//   }]
+function findAllEmp(){
+  axios.get("/findAllEmp").then(function (res){
+    console.log(res.data)
+    form.tableData=res.data.data
+
+  }).catch(function (error){
+    console.log(error)
+  })
+}
+function selectAllEmpByName(){
+  axios.get("/findAllEmpByName?rzname="+form.name).then(function (res){
+    console.log(res.data)
+    form.tableData=res.data.data
+    if(res.data.data.length==0){
+      form.isShow=true
+    }else {
+      form.isShow=false
+    }
+  })
+}
+
+
 
 const selectForm=reactive({
   jxbh:0,
-  jxfs:'',
+  jxfs:0,
+  jdjb:'',
+  jdfs:0
 })
 
 //修改
 const dialogVisible2=ref(false)
 const handleClose2 = (done: () => void) => {
+  ElMessageBox.confirm('确定关闭吗?')
+      .then(() => {
+        done()
+      })
+      .catch(() => {
+        // catch error
+      })
+}
+const dialogVisible3=ref(false)
+const handleClose3 = (done: () => void) => {
   ElMessageBox.confirm('确定关闭吗?')
       .then(() => {
         done()
@@ -172,6 +312,8 @@ function selectOne(res){
   axios.get("/findGradeById?jbh="+res).then(function (res){
     selectForm.jxbh=res.data.data.jxbh
     selectForm.jxfs=res.data.data.jxfs
+    selectForm.jdfs=res.data.data.jdfs
+    selectForm.jdjb=res.data.data.jdjb
     console.log(res.data.data)
   })
 }
@@ -181,20 +323,13 @@ function updateGrade(){
   axios.put("/updateGrade",selectForm).then(function (res){
     console.log(res.data.data)
     selectForm.jxfs=res.data.data.jxfs
+    console.log(selectForm.jxfs)
+    selectForm.jdjb=res.data.data.jdjb
     refresh()
   }).catch(function (error){
     console.log(error)
   })
 }
-// function updateScore(){
-//   axios.put("/updateScore",selectForm).then(function (res){
-//     console.log(res.data)
-//     selectForm.jxbh=res.data.data.jxbh
-//     refresh()
-//   }).catch(function (error){
-//     console.log(error)
-//   })
-// }
 
 //删除评分记录
 const del=()=>{
@@ -215,6 +350,12 @@ const del=()=>{
 }
 
 //消息框
+const open1 = () =>{
+  ElMessage({
+    message:'添加成功!',
+    type:'success'
+  })
+}
 const open2 = () =>{
   ElMessage({
     message:'修改成功!',
@@ -259,6 +400,6 @@ const refresh=inject('reload')
   margin-left: 10px;
 }
 .page{
-  margin-top: 20px;
+  margin-top: 78px;
 }
 </style>
