3c27a3a5c5d548412e0849adabc1de2e919cc703
zwt <3054464773@qq.com>
2022-06-29 20:45:34 +0800

    6.2922

--- src/components/Zpjh.vue
+++ src/components/Zpjh.vue
@@ -321,9 +321,9 @@ function bbb(data){
       <el-table-column prop="zmc"  label="招聘计划内容" />
       <el-table-column  label="审核状态" >
         <template #default="scope">
-          <span v-if="scope.row.shjlzt==1">未审核</span>
-          <span v-else-if="scope.row.shjlzt==2">通过</span>
-          <span v-else-if="scope.row.shjlzt==3">未通过</span>
+          <span v-if="scope.row.shjlzt==1">通过</span>
+          <span v-else-if="scope.row.shjlzt==2">未通过</span>
+          <span v-else-if="scope.row.shjlzt==3">未审核</span>
         </template>
       </el-table-column>
 
